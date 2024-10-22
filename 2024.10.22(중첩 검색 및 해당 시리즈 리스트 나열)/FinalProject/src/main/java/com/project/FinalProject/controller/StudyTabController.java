be2e65fd55d9b0a086b7b618cb0abd8903b4e07f
ohayo <qkzkel17@naver.com>
2024-10-22 18:27:54 +0900

    2024.10.22

---
+++ 2024.10.22(중첩 검색 및 해당 시리즈 리스트 나열)/FinalProject/src/main/java/com/project/FinalProject/controller/StudyTabController.java
@@ -0,0 +1,63 @@
+package com.project.FinalProject.controller;
+
+import java.util.List;
+import org.hibernate.query.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.FinalProject.domain.StudyTab;
+import com.project.FinalProject.service.StudyTabService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Controller
+public class StudyTabController {
+
+	@Autowired
+	StudyTabService studyTabService; 
+	
+	@RequestMapping("/")
+	private String root() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	private String login() {
+		return "home";
+	}
+	
+	@RequestMapping("/list")
+	private String getList(Model model) {
+		org.springframework.data.domain.Page<StudyTab> pageList =studyTabService.findAll(PageRequest.of(0, 28, Sort.by(Sort.Direction.ASC,"studyKey")));
+		model.addAttribute("pageList", pageList);
+		System.out.println("pageList : " + pageList);
+		return "list";
+	}
+	
+	// 중첩된 검색 가능
+	@GetMapping("/search")
+    public String searchStudies(@RequestParam(value = "pId", required = false) String pId,
+                                @RequestParam(value = "pName", required = false) String pName,
+                                @RequestParam(value = "modality", required = false) String modality,
+                                @RequestParam(value = "startDate", required = false) String startDate,
+                                @RequestParam(value = "endDate", required = false) String endDate,
+                                @RequestParam(value = "reportStatus", required = false) Long reportStatus,
+                                @RequestParam(value = "verifyFlag", required = false) Long verifyFlag,
+                                Model model) {
+
+        
+        
+		List<StudyTab> studies = studyTabService.searchStudies(pId, pName, modality, startDate, endDate, reportStatus, verifyFlag);
+        model.addAttribute("studyTabs", studies);
+        return "searchResult";
+        }
+	
+}
