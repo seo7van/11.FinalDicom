535b0391c48ef448afca9dc19b8b4c1749128c38
ohayo <qkzkel17@naver.com>
2024-11-01 18:20:06 +0900

    2024.11.01(시리즈순서대로정렬(이미지로), 시리즈별로 스크롤로 보기 가능)

---
+++ 2024.11.01(시리즈순서대로정렬(이미지로),시리즈별로 스크롤로 이미지싹다보기 가능)/FinalProject/src/main/java/com/project/FinalProject/repository/StudyTabRepository.java
@@ -0,0 +1,34 @@
+package com.project.FinalProject.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.project.FinalProject.domain.StudyTab;
+
+@Repository
+public interface StudyTabRepository extends JpaRepository<StudyTab, Long> {
+	@Query("SELECT s FROM StudyTab s WHERE " +
+		   "(:pId IS NULL OR s.pId = :pId) AND " +
+		   "(:pName IS NULL OR s.pName = :pName) AND " +
+		   "(:modality IS NULL OR s.modality = :modality) AND " +
+		   "(:startDate IS NULL OR s.studyDate >= :startDate) AND " +
+		   "(:endDate IS NULL OR s.studyDate <= :endDate) AND " +
+		   "(:reportStatus IS NULL OR s.reportStatus = :reportStatus) AND " +
+		   "(:verifyFlag IS NULL OR s.verifyFlag = :verifyFlag)")
+	List<StudyTab> searchStudies(@Param("pId") String pId,
+		                         @Param("pName") String pName,
+		                         @Param("modality") String modality,
+		                         @Param("startDate") String startDate,
+		                         @Param("endDate") String endDate,
+		                         @Param("reportStatus") Long reportStatus,
+		                         @Param("verifyFlag") Long verifyFlag);
+	
+	@Query(value = "select * from STUDYTAB where PID = :pId order by STUDYDATE desc", nativeQuery = true)
+	List<StudyTab> findByPIdOrderByStudyDateDesc(@Param("pId") String pId);
+
+	List<StudyTab> findByPatientKey(Long patientKey);
+	}
---
+++ 2024.11.01(시리즈순서대로정렬(이미지로),시리즈별로 스크롤로 이미지싹다보기 가능)/FinalProject/src/main/java/com/project/FinalProject/service/StudyTabService.java
@@ -0,0 +1,35 @@
+package com.project.FinalProject.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.project.FinalProject.domain.StudyTab;
+import com.project.FinalProject.repository.StudyTabRepository;
+
+@Service
+public class StudyTabService {
+	
+	@Autowired
+	StudyTabRepository studyTabRepository;
+	
+	//기존 findAll 메소드
+	public Page<StudyTab> findAll(PageRequest page){
+		return studyTabRepository.findAll(page);
+		}
+
+	public List<StudyTab> searchStudies(String pId, String pName, String modality, String startDate, String endDate, Long reportStatus, Long verifyFlag) {
+        return studyTabRepository.searchStudies(pId, pName, modality, startDate, endDate, reportStatus, verifyFlag);
+        }
+	
+	public StudyTab getStudyByStudyKey(Long studyKey) {
+		return studyTabRepository.findById(studyKey).orElse(null);
+	}
+
+	public List<StudyTab> getPastStudies(Long patientKey) {
+		return studyTabRepository.findByPatientKey(patientKey);
+	}
+	}
